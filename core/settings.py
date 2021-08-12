e01c00263c04d031515e29f1c0a526188bb11218
AKINTOLA STEPHEN IYANU <sakintola351@gmail.com>
2021-08-12 23:08:35 +0100

    some changes made

--- core/settings.py
+++ core/settings.py
@@ -4,6 +4,7 @@ import os
 from decouple import config
 from unipath import Path
 import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = Path(__file__).parent
@@ -16,7 +17,9 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 # load production server from .env
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
+ALLOWED_HOSTS = ['localhost', 'dm-predictor.herokuapp.com', '127.0.0.1', config('SERVER', default='127.0.0.1')]
+
+
 
 # Application definition
 
@@ -67,12 +70,27 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME'  : 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME'  : 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME'  : 'dca0jfsemklnkt',
+        'USER' : 'nqvfllwffugkjz',
+        'PASSWORD': 'a53f5093cf614ea0bee56401245b1e5e95de75ad1e2792ca2732997619fc87d0',
+        'HOST': 'ec2-52-45-183-77.compute-1.amazonaws.com',
+        'PORT': '5432'
     }
 }
+
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -120,6 +138,12 @@ STATICFILES_DIRS = (
 )
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+
+django_heroku.settings(locals())
 
 #############################################################
 #############################################################
